d978a7234630d5974556884959a03bff2a118c1b
Diarmuid <20071587@mail.wit.ie>
2024-11-24 16:14:37 +0000

    Add readNext functionality in UserInput

--- src/main/kotlin/utils/UserInput.kt
+++ src/main/kotlin/utils/UserInput.kt
@@ -20,12 +20,34 @@ fun readNextLine(prompt: String?): String {
 
 
 fun readNextChar(prompt: String?): Char {
-    do {
-        try {
+    do{
+        try{
             print(prompt)
             return readln().first()
-        } catch (e: NumberFormatException) {
+        } catch (e: Exception) {
             System.err.println("\tEnter a character please.")
+        }
+    } while (true)
+}
+
+fun readNextDouble(prompt: String?): Double{
+    do{
+        try{
+            print(prompt)
+            return readln().toDouble()
+        } catch (e: NumberFormatException){
+            System.err.println("\tEnter a number please")
+        }
+    } while (true)
+}
+
+fun readNextFloat(prompt: String?): Float{
+    do{
+        try{
+            print(prompt)
+            return readln().toFloat()
+        } catch (e: NumberFormatException){
+            System.err.println("\tEnter a number please")
         }
     } while (true)
 }
